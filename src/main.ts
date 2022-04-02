9bb9bb792e2f0aaa12a4df84055e61102a33438b
Jose Daniel Parra <jose-paper@hotmail.com>
2022-04-02 14:08:23 -0500

    Remove vue spinner, addition of stylus

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { Quasar } from 'quasar'
 import { createPinia } from 'pinia'
 import router from "./router";
-import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
 import App from './App.vue'
 // Import icon libraries
 import '@quasar/extras/material-icons/material-icons.css'
@@ -10,4 +9,4 @@ import '@quasar/extras/material-icons/material-icons.css'
 // Import Quasar css
 import 'quasar/src/css/index.sass'
 
-createApp(App).use(router).use(Quasar).use(createPinia()).component("PulseLoader", PulseLoader).mount('#app');
+createApp(App).use(router).use(Quasar).use(createPinia()).mount('#app');
